6377a2a800f77d2b598ac9d597fe44a51680f2f8
declanm.mulligan@gmail.com Mulligan <declanm.mulligan@gmail.com>
2017-12-17 20:37:36 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,7 +21,15 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-
+	monopoly = {}
+	monopoly[:railroads]={
+		pieces:4,names:{},
+		rent_in_dollars:{
+			one_piece_owned:25
+			two_pieces_owned:50
+		}
+	}
+	monopoly
 
 end
 
